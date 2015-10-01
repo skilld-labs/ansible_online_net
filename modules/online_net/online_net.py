6e8cee35cd66439a382cb887454238ba65e9b842
Jean-Baptiste Guerraz <jbguerraz@skilld.fr>
2015-10-02 00:07:57 +0200

    added authors

--- modules/online_net/online_net.py
+++ modules/online_net/online_net.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+######################################################################
+# (c) 2015, Jean-Baptiste Guerraz <jbguerraz@gmail.com>,
+#           Andrey Postnikov <apostnikov@gmail.com>
+#
 # This file is part of Ansible
 #
 # Ansible is free software: you can redistribute it and/or modify
@@ -15,6 +18,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
 DOCUMENTATION = '''
 ---
 module: online_net
